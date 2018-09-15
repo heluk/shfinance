f8781b956f5fb1660a73c1c2304c6203b81f2b74
heluk <l.heschl@gmail.com>
2018-09-16 00:37:24 +0200

    update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Link from "gatsby-link";
 import "../assets/scss/main.scss";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header";
+import Footer from "../components/hooter";
 
 class Template extends React.Component {
     constructor(props) {
