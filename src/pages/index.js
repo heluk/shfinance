a1e92ff1843d6733de00403c1745f8682e68ad61
heluk <l.heschl@gmail.com>
2018-09-16 12:43:14 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/business.jpg'
 import pic03 from '../assets/images/investment2.jpg'
 import pic04 from '../assets/images/realestate.jpg'
-import OttoHeschl from '../assets/images/OttoHeschl.jpg'
+import OttoHeschl from '../assets/images/profil.jpg'
 
 class Homepage extends React.Component {
     render() {
@@ -43,28 +43,16 @@ class Homepage extends React.Component {
 
                     </div>
                 </section>
-
-                <section id="two" className="main style2">
-                    <div className="grid-wrapper">
-                        <div className="col-6">
-                            <ul className="major-icons">
-                                <li><span className="icon style1 major fa-code"></span></li>
-                                <li><span className="icon style2 major fa-bolt"></span></li>
-                                <li><span className="icon style3 major fa-camera-retro"></span></li>
-                                <li><span className="icon style4 major fa-cog"></span></li>
-                                <li><span className="icon style5 major fa-desktop"></span></li>
-                                <li><span className="icon style6 major fa-calendar"></span></li>
-                            </ul>
-                        </div>
-                        <div className="col-6">
-                            <header className="major">
-                                <h2>Lorem ipsum dolor adipiscing<br />
-                                amet dolor consequat</h2>
-                            </header>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                            <p>Blandit faucibus proin. Ac aliquam integer adipiscing enim non praesent vis commodo nunc phasellus cubilia ac risus accumsan. Accumsan blandit. Lobortis phasellus non lobortis dit varius mi varius accumsan lobortis. Blandit ante aliquam lacinia lorem lobortis semper morbi col faucibus vitae integer placerat accumsan orci eu mi odio tempus adipiscing adipiscing adipiscing curae consequat feugiat etiam dolore.</p>
-                            <p>Adipiscing a commodo ante nunc accumsan interdum mi ante adipiscing. A nunc lobortis non nisl amet vis volutpat aclacus nascetur ac non. Lorem curae eu ante amet sapien in tempus ac. Adipiscing id accumsan adipiscing ipsum.</p>
-                        </div>
+                
+                <section id="contact" className="main style2 special">
+                    <div className="container">
+                        <header className="major">
+                            <h2>Interesse?</h2>
+                        </header>
+                        <p>Wir beraten Sie gerne.</p>
+                        <ul className="actions uniform">
+                            <li><a href="tel:+43 512 57 11 27" className="button special">+43 512 57 11 27</a></li>
+                        </ul>
                     </div>
                 </section>
 
@@ -80,19 +68,6 @@ class Homepage extends React.Component {
                         <div className="col-6">
                             <span className="image fit"><img src={OttoHeschl} alt="" /></span>
                         </div>
-                    </div>
-                </section>
-                
-                <section id="Kontakt" className="main style2 special">
-                    <div className="container">
-                        <header className="major">
-                            <h2>Ipsum feugiat consequat?</h2>
-                        </header>
-                        <p>Sed lacus nascetur ac ante amet sapien.</p>
-                        <ul className="actions uniform">
-                            <li><a href="#" className="button special">Sign Up</a></li>
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
                     </div>
                 </section>
             </div>
